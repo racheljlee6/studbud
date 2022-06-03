bba887772e7c86cafc3b4d52db0b33caf850e2cb
racheljlee6 <89394433+racheljlee6@users.noreply.github.com>
2022-06-03 12:19:29 +1000

    timetools - styled, working and responsive

--- dist/timetools.3b06b0f7.js
+++ dist/timetools.3b06b0f7.js
@@ -507,8 +507,9 @@ function hmrAcceptRun(bundle, id) {
 var _timetools = require("./components/timetools");
 
 },{"./components/timetools":"7uux5"}],"7uux5":[function(require,module,exports) {
+// STOPWATCH JS //
 //added hour to stopwatch based on user feedback
-let [sec, min, hr] = [
+let [secs, mins, hr] = [
     0,
     0,
     0
@@ -527,7 +528,7 @@ document.getElementById("stopTimer").addEventListener("click", ()=>{
 });
 document.getElementById("resetTimer").addEventListener("click", ()=>{
     clearInterval(int);
-    [sec, min, hr] = [
+    [secs, mins, hr] = [
         0,
         0,
         0
@@ -536,20 +537,118 @@ document.getElementById("resetTimer").addEventListener("click", ()=>{
 });
 //cycle through time
 function cycleStopwatch() {
-    sec++;
-    if (sec >= 60) {
-        sec = 0;
-        min++;
-        if (min >= 60) {
-            min = 0;
+    secs++;
+    if (secs >= 60) {
+        secs = 0;
+        mins++;
+        if (mins >= 60) {
+            mins = 0;
             hr++;
         }
     }
+    // conditional operatation
     let h = hr < 10 ? "0" + hr : hr;
-    let m = min < 10 ? "0" + min : min;
-    let s = sec < 10 ? "0" + sec : sec;
+    let m = mins < 10 ? "0" + mins : mins;
+    let s = secs < 10 ? "0" + secs : secs;
+    // modify content of HTML element through innerhtml
     defaultTime.innerHTML = `${h} : ${m} : ${s}`;
 }
+// POMODORO JS //
+// variables for timer based on each feature time period
+const timer = {
+    pomodoro: 25,
+    shortBreak: 5,
+    longBreak: 30
+};
+let interval;
+const startStop = document.getElementById("pomoButton");
+startStop.addEventListener("click", ()=>{
+    const { action  } = startStop.dataset;
+    if (action === "start") startPomo();
+    else stopPomo();
+});
+// function to update countdown with clicked time 
+const pomodoroModes = document.querySelector("#pomodoro-modes");
+pomodoroModes.addEventListener("click", handleMode);
+// function to find diff between current time and end time and return in milliseconds
+function getTimeLeft(endT) {
+    //retriveve exact time of current moment
+    // where currentT = current time the clock is at
+    const currentT = Date.parse(new Date());
+    const diff = endT - currentT;
+    // converting to base 10
+    // where t = total, m = minutes and s= seconds.
+    const t = Number.parseInt(diff / 1000, 10);
+    const minute = Number.parseInt(t / 60 % 60, 10);
+    const second = Number.parseInt(t % 60, 10);
+    return {
+        t,
+        minute,
+        second
+    };
+}
+// start time by count down to 0
+function startPomo() {
+    let { t  } = timer.remainingTime;
+    // where endT = time the clock will end
+    const endT = Date.parse(new Date()) + t * 1000;
+    startStop.dataset.action = "stop";
+    startStop.textContent = "stop";
+    startStop.classList.add("active");
+    interval = setInterval(function() {
+        // update clock time with new time
+        timer.remainingTime = getTimeLeft(endT);
+        clockUpdate();
+        //stop timer when reach 0 
+        t = timer.remainingTime.t;
+        if (t <= 0) clearInterval(interval);
+    }, 1000);
+}
+// stop timer when clicked
+function stopPomo() {
+    clearInterval(interval);
+    startStop.dataset.action = "start";
+    startStop.textContent = "start";
+    startStop.classList.remove("active");
+}
+//update countdown clock
+function clockUpdate() {
+    const { remainingTime  } = timer;
+    // https://www.w3schools.com/js/js_string_methods.asp
+    // pads a string withh another string
+    const minute = `${remainingTime.minute}`.padStart(2, "0");
+    const second = `${remainingTime.second}`.padStart(2, "0");
+    const min = document.getElementById("pomoMins");
+    const sec = document.getElementById("pomoSecs");
+    // return text content of specified element
+    min.textContent = minute;
+    sec.textContent = second;
+}
+// change betwwen modes
+function changeMode(mode) {
+    timer.mode = mode;
+    timer.remainingTime = {
+        t: timer[mode] * 60,
+        minute: timer[mode],
+        second: 0
+    };
+    //remove active from mode buttons and set for clicked button.
+    document.querySelectorAll("button[data-mode]").forEach((e)=>e.classList.remove("active"));
+    document.querySelector(`[data-mode="${mode}"]`).classList.add("active");
+    clockUpdate();
+}
+// retrieve data-mode attribute value 
+//if data-mode attribute doesnt exisit the target was not one of the buttons and the function ends 
+function handleMode(event) {
+    const { mode  } = event.target.dataset;
+    if (!mode) return;
+    changeMode(mode);
+    stopPomo();
+}
+// make default mode to be pomodoro and reset values
+document.addEventListener("DOMContentLoaded", ()=>{
+    changeMode("pomodoro");
+});
 
 },{}]},["lNJce","2OD7o"], "2OD7o", "parcelRequire60da")
 
